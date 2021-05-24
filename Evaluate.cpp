e2af5ad7d0558bb6d154bdfe60fb30d3adbfdc04
Ciorap88 <vlad.014916@gmail.com>
2021-05-24 10:33:13 +0300

    Added king safety and queen evals

--- Evaluate.cpp
+++ Evaluate.cpp
@@ -83,6 +83,7 @@ const int trappedBishopPenalty = 150;
 const int fianchettoBonus = 30;
 const int bishopMobilityConstant = 5;
 const int attackingBishopBonus = 5;
+const int blockedBishopPenalty = 30;
 
 int evalBishop(int sq, int color) {
     U64 ourPawnsBB = (board.whitePiecesBB & board.pawnsBB);
@@ -97,18 +98,28 @@ int evalBishop(int sq, int color) {
 
     int eval = pieceValues[Bishop];
 
-    // traps
+    // traps and blockages
     if(color == White) {
         if(sq == a7 && (opponentPawnsBB & bits[b6]) && (opponentPawnsBB & bits[c7]))
             eval -= trappedBishopPenalty;
         if(sq == h7 && (opponentPawnsBB & bits[g6]) && (opponentPawnsBB & bits[f7]))
             eval -= trappedBishopPenalty;
+
+        if(sq == c1 && (ourPawnsBB & bits[d2]) & (ourPiecesBB & bits[d3]))
+            eval -= blockedBishopPenalty;
+        if(sq == f1 && (ourPawnsBB & bits[e2]) & (ourPiecesBB & bits[e3]))
+            eval -= blockedBishopPenalty;
     }
     if(color == Black) {
         if(sq == a2 && (opponentPawnsBB & bits[b3]) && (opponentPawnsBB & bits[c2]))
             eval -= trappedBishopPenalty;
         if(sq == h2 && (opponentPawnsBB & bits[g3]) && (opponentPawnsBB & bits[f2]))
             eval -= trappedBishopPenalty;
+
+        if(sq == c8 && (ourPawnsBB & bits[d7]) & (ourPiecesBB & bits[d6]))
+            eval -= blockedBishopPenalty;
+        if(sq == f8 && (ourPawnsBB & bits[e7]) & (ourPiecesBB & bits[e6]))
+            eval -= blockedBishopPenalty;
     }
 
     // fianchetto bonus (bishop on long diagonal on the second rank)
@@ -124,7 +135,8 @@ int evalBishop(int sq, int color) {
         int curSq = sq+dir;
         while(true) {
             if(!(bits[curSq] & ourPiecesBB)) mobility++;
-            if(opponentKingSquare == curSq || (kingAttacksBB[opponentKingSquare] & bits[curSq])) attackedSquares++;
+            if(opponentKingSquare == curSq || (kingAttacksBB[opponentKingSquare] & bits[curSq]))
+                attackedSquares++;
 
             if(!isInBoard(curSq, dir) || (bits[curSq] & (opponentPiecesBB | ourPiecesBB))) break;
             curSq += dir;
@@ -143,6 +155,7 @@ const int rookPawnConstant = 2;
 const int rooksDefendingEachOther = 5;
 const int attackingRookBonus = 10;
 const int rookMobilityConstant = 3;
+const int blockedRookPenalty = 30;
 
 int evalRook(int sq, int color) {
     U64 currFileBB = filesBB[sq%8];
@@ -161,6 +174,20 @@ int evalRook(int sq, int color) {
     int eightRank = (color == White ? 7 : 0);
 
     int eval = pieceValues[Rook];
+
+    // blocked by uncastled king
+    if(color == White) {
+        if((board.whiteKingSquare == f1 || board.whiteKingSquare == g1) && (sq == g1 || sq == h1))
+            eval -= blockedRookPenalty;
+        if((board.whiteKingSquare == c1 || board.whiteKingSquare == b1) && (sq == a1 || sq == b1))
+            eval -= blockedRookPenalty;
+    }
+    if(color == Black) {
+        if((board.whiteKingSquare == f8 || board.whiteKingSquare == g8) && (sq == g8 || sq == h8))
+            eval -= blockedRookPenalty;
+        if((board.whiteKingSquare == c8 || board.whiteKingSquare == b8) && (sq == a8 || sq == b8))
+            eval -= blockedRookPenalty;
+    }
 
     // the rook becomes more valuable as there are less pawns on the board
     int numberOfPawns = popcount(board.pawnsBB);
@@ -192,7 +219,8 @@ int evalRook(int sq, int color) {
         int curSq = sq+dir;
         while(true) {
             if(!(bits[curSq] & ourPiecesBB)) mobility++;
-            if(opponentKingSquare == curSq || (kingAttacksBB[opponentKingSquare] & bits[curSq])) attackedSquares++;
+            if(opponentKingSquare == curSq || (kingAttacksBB[opponentKingSquare] & bits[curSq]))
+                attackedSquares++;
 
             if(!isInBoard(curSq, dir) || (bits[curSq] & (opponentPiecesBB | ourPiecesBB))) break;
             curSq += dir;
@@ -202,5 +230,134 @@ int evalRook(int sq, int color) {
     eval += rookMobilityConstant * (mobility-7);
     eval += attackingRookBonus * attackedSquares;
 
+    return eval;
+}
+
+const int attackingQueenBonus = 15;
+const int earlyQueenDevelopment = 5;
+const int queenMobilityConstant = 3;
+
+int evalQueen(int sq, int color) {
+    U64 ourPiecesBB = (color == White ? board.whitePiecesBB : board.blackPiecesBB);
+    U64 opponentPiecesBB = (color == Black ? board.whitePiecesBB : board.blackPiecesBB);
+    U64 ourBishopsBB = (board.bishopsBB & ourPiecesBB);
+    U64 ourKnightsBB = (board.knightsBB & ourPiecesBB);
+
+    int opponentKingSquare = (color == Black ? board.whiteKingSquare : board.blackKingSquare);
+
+    int eval = pieceValues[Queen];
+
+    // penalty for early development
+    if(color == White && sq/8 > 1) {
+        if(ourKnightsBB & bits[b1]) eval -= earlyQueenDevelopment;
+        if(ourBishopsBB & bits[c1]) eval -= earlyQueenDevelopment;
+        if(ourBishopsBB & bits[f1]) eval -= earlyQueenDevelopment;
+        if(ourKnightsBB & bits[g1]) eval -= earlyQueenDevelopment;
+    }
+    if(color == Black && sq/8 < 6) {
+        if(ourKnightsBB & bits[b8]) eval -= earlyQueenDevelopment;
+        if(ourBishopsBB & bits[c8]) eval -= earlyQueenDevelopment;
+        if(ourBishopsBB & bits[f8]) eval -= earlyQueenDevelopment;
+        if(ourKnightsBB & bits[g8]) eval -= earlyQueenDevelopment;
+    }
+
+    // mobility and squares attacked near the opponent king
+    int mobility = 0;
+    int attackedSquares = 0;
+    for(int dir: piecesDirs[Queen]) {
+        if(!isInBoard(sq, dir)) continue;
+
+        int curSq = sq+dir;
+        while(true) {
+            if(!(bits[curSq] & ourPiecesBB)) mobility++;
+            if(opponentKingSquare == curSq || (kingAttacksBB[opponentKingSquare] & bits[curSq]))
+                attackedSquares++;
+
+            if(!isInBoard(curSq, dir) || (bits[curSq] & (opponentPiecesBB | ourPiecesBB))) break;
+            curSq += dir;
+        }
+    }
+
+    eval += queenMobilityConstant * (mobility-14);
+    eval += attackingQueenBonus * attackedSquares;
+
+    return eval;
+}
+
+const int shield1 = 10;
+const int shield2 = 5;
+const int noShield = 5;
+
+int whiteKingShield() {
+    U64 ourPawnsBB = (board.whitePiecesBB & board.pawnsBB);
+    int sq = board.whiteKingSquare;
+
+    int eval = 0;
+    // queen side
+    if(sq%8 < 3) {
+        if(ourPawnsBB & bits[a2]) eval += shield1;
+        else if(ourPawnsBB & bits[a3]) eval += shield2;
+        else eval -= noShield;
+
+        if(ourPawnsBB & bits[b2]) eval += shield1;
+        else if(ourPawnsBB & bits[b3]) eval += shield2;
+        else eval -= noShield;
+
+        if(ourPawnsBB & bits[c2]) eval += shield1;
+        else if(ourPawnsBB & bits[c3]) eval += shield2;
+        else eval -= noShield;
+    }
+
+    // king side
+    else if(sq%8 > 4) {
+        if(ourPawnsBB & bits[f2]) eval += shield1;
+        else if(ourPawnsBB & bits[f3]) eval += shield2;
+        else eval -= noShield;
+
+        if(ourPawnsBB & bits[g2]) eval += shield1;
+        else if(ourPawnsBB & bits[g3]) eval += shield2;
+        else eval -= noShield;
+
+        if(ourPawnsBB & bits[h2]) eval += shield1;
+        else if(ourPawnsBB & bits[h3]) eval += shield2;
+        else eval -= noShield;
+    }
+    return eval;
+}
+
+int blackKingShield() {
+    U64 ourPawnsBB = (board.blackPiecesBB & board.pawnsBB);
+    int sq = board.blackKingSquare;
+
+    int eval = 0;
+    // queen side
+    if(sq%8 < 3) {
+        if(ourPawnsBB & bits[a7]) eval += shield1;
+        else if(ourPawnsBB & bits[a6]) eval += shield2;
+        else eval -= noShield;
+
+        if(ourPawnsBB & bits[b7]) eval += shield1;
+        else if(ourPawnsBB & bits[b6]) eval += shield2;
+        else eval -= noShield;
+
+        if(ourPawnsBB & bits[c7]) eval += shield1;
+        else if(ourPawnsBB & bits[c6]) eval += shield2;
+        else eval -= noShield;
+    }
+
+    // king side
+    else if(sq%8 > 4) {
+        if(ourPawnsBB & bits[f7]) eval += shield1;
+        else if(ourPawnsBB & bits[f6]) eval += shield2;
+        else eval -= noShield;
+
+        if(ourPawnsBB & bits[g7]) eval += shield1;
+        else if(ourPawnsBB & bits[g6]) eval += shield2;
+        else eval -= noShield;
+
+        if(ourPawnsBB & bits[h7]) eval += shield1;
+        else if(ourPawnsBB & bits[h6]) eval += shield2;
+        else eval -= noShield;
+    }
     return eval;
 }
