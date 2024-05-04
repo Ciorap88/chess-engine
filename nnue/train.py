3a87f11f9800b5d631a44b1602d379242fc9fc2b
Vlad Ciocoiu <vlad.014916@gmail.com>
2024-05-04 16:18:10 +0300

    Add lr scheduler

--- nnue/train.py
+++ nnue/train.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 import numpy as np
 import ctypes
 import random
@@ -26,13 +27,20 @@ if __name__ == "__main__":
     model = NNUE(batches_t[0][0].size(1), 64, 64, 1).to(0)
     
     optimizer = optim.Adam(model.parameters(), lr=0.05)
+    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=2, verbose=True)    
     
+    best_loss = float('inf')
+    no_improvement_count = 0
+    patience = 5
+        
     # Step 5: Write the training loop
-    num_epochs = 10
+    num_epochs = 35
     for epoch in range(num_epochs):
         random.shuffle(batches_t)
+        total_loss = 0
+        
         for batch_idx, batch in enumerate(batches_t):
-            print(f"Epoch {epoch}/{num_epochs}: batch {batch_idx}/{numberOfBatches}...", end='\r')
+            print(f"Epoch {epoch+1}/{num_epochs}: batch {batch_idx}/{numberOfBatches}...", end='\r')
             
             white_features_t, black_features_t, stm_t, score_t, result_t = batch
             
@@ -41,15 +49,26 @@ if __name__ == "__main__":
             
             # Compute the loss
             loss_val = loss.CrossEntropy(output, score_t, result_t)
+            total_loss += loss_val / numberOfBatches
             
             # Backward pass
             optimizer.zero_grad()
             loss_val.backward()
             optimizer.step()
+            
+        print(f"\nEpoch {epoch+1}/{num_epochs} done, loss={round(total_loss.item(), 5)}")
+        
+        if total_loss.item() < best_loss:
+            best_loss = total_loss.item()
+            no_improvement_count = 0
+        else:
+            no_improvement_count += 1
+        
+        if no_improvement_count >= patience:
+            print(f"No improvement in loss for {patience} epochs. Exiting training loop.")
+            break
         
             
-        # Print progress
-        print(f"\nEpoch {epoch+1}/{num_epochs} done, loss={round(loss_val.item(), 5)}")
 
     torch.save(model.state_dict(), 'nnue_trained.pth')
     print("Model weights saved successfully to nnue_trained.pth.")
